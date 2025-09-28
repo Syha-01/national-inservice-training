964256c54b8282c08a4f0e96068123f8c039d635
Enrisen Tzib <enrisentzib@gmail.com>
2025-09-28 14:27:56 -0600

    Starting with the actual API

---
+++ cmd/api/nits.go
@@ -0,0 +1,4 @@
+package main
+
+//slide 94 starting with the comments
+//https://docs.google.com/presentation/d/1o-pz5Y5Ftd7thjTgrnYI9brzorg6F8UlG1IoefydLII/edit?slide=id.g2d3dde2177b_0_81#slide=id.g2d3dde2177b_0_81
