5c24a9403eae2f4a3ec8bfe9b78b9bf0a8e77ddc
Enrisen Tzib <enrisentzib@gmail.com>
2025-09-28 14:04:24 -0600

    complete json v3

--- cmd/api/healtcheck.go
+++ cmd/api/healtcheck.go
@@ -1,21 +1,18 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
 func (a *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	data := map[string]string{"status": "available", "environment": a.config.env, "version": version}
-
-	jsResponse, err := json.Marshal(data)
+	data := map[string]string{
+		"status":      "available",
+		"environment": a.config.env,
+		"version":     version,
+	}
+	err := a.writeJSON(w, http.StatusOK, data, nil)
 	if err != nil {
 		a.logger.Error(err.Error())
 		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
-		return
 	}
-	jsResponse = append(jsResponse, '\n')
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(jsResponse)
-
 }
