1685a53bca17c6ad26c9400e59a2eaf45ca02d7a
Enrisen Tzib <enrisentzib@gmail.com>
2025-09-28 14:02:06 -0600

    Json v2

--- cmd/api/healtcheck.go
+++ cmd/api/healtcheck.go
@@ -1,13 +1,21 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 func (a *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	jsResponse := `{"status": "available", "environment": %q, "version": %q}`
-	jsResponse = fmt.Sprintf(jsResponse, a.config.env, version)
+	data := map[string]string{"status": "available", "environment": a.config.env, "version": version}
+
+	jsResponse, err := json.Marshal(data)
+	if err != nil {
+		a.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		return
+	}
+	jsResponse = append(jsResponse, '\n')
 	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(jsResponse))
+	w.Write(jsResponse)
+
 }
